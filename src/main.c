078f8cbfb3719347e7fc097bb939eb7d92d0c95c
Adrián Vizcaíno <v.r.adrian96@gmail.com>
2022-06-16 13:57:22 +0100

    add station library

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include "esp_log.h"
 #include "driver/i2c.h"
 #include "sdkconfig.h"
-
+#include "station.h"
 #include "tasks.h"
 
 rgb_sensor *rgb;
@@ -10,9 +10,33 @@ rgb_sensor *rgb;
 
 void app_main() 
 {
-
+    /* I2C master initializaction */
     ESP_ERROR_CHECK(i2c_master_init());
+
+    /* Connect esp32 to a wifi network (station) */
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    printf("ESP_WIFI_MODE_STA\n");
+    wifi_init_sta();
+
+    /* MQQT start and conected to AWS broker */
+
+    
+
+    /* I2C Switch Task Created */
+
+    /* S13683 Task Created */
     xTaskCreate(s13673_task, "i2c_test_task_0", 1024 * 2, &rgb, 10, NULL);
+
+    /* Oled Task Created */
+
+    /* Buttons Task Created */
 }
 
 
